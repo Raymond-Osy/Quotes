bda4b5d12e6f4e8cc1c4a426e26d776b12086c68
Raymond Osy <raymondosy1@gmail.com>
2018-08-16 00:38:37 +0100

    Add some functionalities

--- app.js
+++ app.js
@@ -1,2 +1,12 @@
+var quotes = [
+    "Life will hit you up and throw you down, what matters is that you rise- Seni Sulyman",
+    "Live each day as if it was the last- Steve Jobs",
+    "Be careful how you talk to yourself. You are always listening."
+];
 
 
+var changeBtn = document.getElementById("change");
+changeBtn.onclick = function randomQuote(){
+    var randomQuote = Math.floor(Math.random() * quotes.length);
+    document.getElementById("random").innerHTML = quotes[randomQuote];
+  };
