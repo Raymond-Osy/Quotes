317bbfc2a84138de7bf7ef55b9970382f15088c9
Raymond Osy <raymondosy1@gmail.com>
2018-08-17 02:00:30 +0100

    Add images in app.js, Add styles for app and modify the html

--- app.js
+++ app.js
@@ -4,16 +4,32 @@ var quotes = [
     "Be careful how you talk to yourself. You are always listening."
 ];
 
+// Add array for images as random image directly from splash will reqire page to be refreshed which in turn will clear previous stored array
+var images = [
+    "https://source.unsplash.com/tGTVxeOr_Rs/1600x900",
+    "https://source.unsplash.com/sMQiL_2v4vs/1600x900",
+    "https://source.unsplash.com/zNN6ubHmruI/1600x900",
+    "https://source.unsplash.com/XiYUzsml06M/1600x900",
+    "https://source.unsplash.com/salaAJW7Xdg/1600x900",
+    "https://source.unsplash.com/HcmdstM9IFw/1600x900",
+    "https://source.unsplash.com/xr-y6Ruw7K8/1600x900",
+    "https://source.unsplash.com/XnvLe0u9iM8/1600x900",
+    "https://source.unsplash.com/Hn8N4I4eHA0/1600x900",
+    "https://source.unsplash.com/UBP-Cp0cnYU/1600x900",
+]
+// add condition for empty inbox
 
 var changeBtn = document.getElementById("change");
 changeBtn.onclick = function randomQuote(){
     var randomQuote = Math.floor(Math.random() * quotes.length);
+    var randomImage = Math.floor(Math.random() * images.length);
     document.getElementById("random").innerHTML = quotes[randomQuote];
+    //add img to html and grab it here
   };
 
 var addQuoteBtn = document.getElementById("addQuote");
 addQuoteBtn.onclick = function newQuote(){
     var input = document.getElementById("input").value;
     quotes.push(input);
-    var input = document.getElementById("input").value = "";
+    document.getElementById("input").value = "";
 };
