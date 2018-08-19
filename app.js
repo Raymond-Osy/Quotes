e6b6621ac0e21d91204ac8cd5026922bde2d4ee3
Raymond Osy <raymondosy1@gmail.com>
2018-08-19 22:11:31 +0100

    Modified the image links for faster loading

--- app.js
+++ app.js
@@ -6,16 +6,16 @@ var quotes = [
 
 // Add array for images as random image directly from splash will reqire page to be refreshed which in turn will clear previous stored array
 var images = [
-    "https://source.unsplash.com/tGTVxeOr_Rs/1600x900",
-    "https://source.unsplash.com/sMQiL_2v4vs/1600x900",
-    "https://source.unsplash.com/zNN6ubHmruI/1600x900",
-    "https://source.unsplash.com/XiYUzsml06M/1600x900",
-    "https://source.unsplash.com/salaAJW7Xdg/1600x900",
-    "https://source.unsplash.com/HcmdstM9IFw/1600x900",
-    "https://source.unsplash.com/xr-y6Ruw7K8/1600x900",
-    "https://source.unsplash.com/XnvLe0u9iM8/1600x900",
-    "https://source.unsplash.com/Hn8N4I4eHA0/1600x900",
-    "https://source.unsplash.com/UBP-Cp0cnYU/1600x900",
+    "https://images.unsplash.com/photo-1502082553048-f009c37129b9?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1600&h=900&fit=crop&ixid=eyJhcHBfaWQiOjF9&s=3cd63c34901a7ebc5b53539d0223112d",
+    "https://images.unsplash.com/photo-1453791052107-5c843da62d97?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1600&h=900&fit=crop&ixid=eyJhcHBfaWQiOjF9&s=c8a126805897fad59b03a5d39bafdf0b",
+    "https://images.unsplash.com/photo-1476610182048-b716b8518aae?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1600&h=900&fit=crop&ixid=eyJhcHBfaWQiOjF9&s=51ab375b3b38b50fb72c1b8113d3ff73",
+    "https://images.unsplash.com/photo-1476608825565-f7cebeca38b1?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1600&h=900&fit=crop&ixid=eyJhcHBfaWQiOjF9&s=c730c2efc7bcdd33569ad2b55f9ebd3d",
+    "https://images.unsplash.com/photo-1506269351850-0428eaed2193?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1600&h=900&fit=crop&ixid=eyJhcHBfaWQiOjF9&s=bdb956f719cc943bf385f89507e68ac9",
+    "https://images.unsplash.com/photo-1511151083847-62cedb3a5ea2?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1600&h=900&fit=crop&ixid=eyJhcHBfaWQiOjF9&s=303427d5268d28956da41b090e50ccd6",
+    "https://images.unsplash.com/photo-1516939884455-1445c8652f83?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1600&h=900&fit=crop&ixid=eyJhcHBfaWQiOjF9&s=60bbdef6bb8c2e69133a01ad42d1731e",
+    "https://images.unsplash.com/photo-1446769066069-f8c54fae653b?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1600&h=900&fit=crop&ixid=eyJhcHBfaWQiOjF9&s=449dce083f3b035519113cbb162dea87",
+    "https://images.unsplash.com/photo-1509529711801-deac231925ac?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1600&h=900&fit=crop&ixid=eyJhcHBfaWQiOjF9&s=6f2b3d61a8d897fd3294168be6eb8198",
+    "https://images.unsplash.com/photo-1507644980989-d116ca3c5d89?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1600&h=900&fit=crop&ixid=eyJhcHBfaWQiOjF9&s=22a85c8396697eb5d89dbbd1c7faafba",
 ]
 // add condition for empty inbox
 
